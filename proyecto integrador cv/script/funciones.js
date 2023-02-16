511452651803095386fea85476fbdbade2236b11
miguelbrs <123672150+miguelbrs@users.noreply.github.com>
2023-02-15 22:19:21 -0300

    Add files via upload

---
+++ proyecto integrador cv/script/funciones.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("tituloh1").innerHTML="PORTFOLIO WEB de Julio Esteban Gomez "
+document.getElementById("tituloh1").style.color="#140331"
+
+//PORTFOLIO WEB de Julio Esteban Gomez
